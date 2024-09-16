0304b5aa4a11f1abaf684398df8a93315764d27e
Tamtun <165990188+Tamtun@users.noreply.github.com>
2024-09-16 15:58:34 +0700

    Update lesson5_task6.py

--- lesson5/05__lesson/lesson5_task6.py
+++ lesson5/05__lesson/lesson5_task6.py
@@ -8,24 +8,17 @@ from time import sleep
 options = Options()
 options.headless = False  # Запуск браузера с интерфейсом
 
-# Путь к geckodriver
-service = Service(
-    executable_path="C:\\Users\\intes\\OneDrive\\Рабочий стол\\python lessons\\lesson5\\geckodriver.exe"
-)
-
 # Инициализация браузера с geckodriver
-driver = webdriver.Firefox(service=service, options=options)
+driver = webdriver.Firefox()
 
 # Открываем страницу
 driver.get("http://the-internet.herokuapp.com/login")
 
 # Находим поле ввода username и вводим значение
-username_field = driver.find_element(By.ID, "username")
-username_field.send_keys("tomsmith")
+username_field = driver.find_element(By.ID, "username").send_keys("tomsmith")
 
 # Находим поле ввода password и вводим значение
-password_field = driver.find_element(By.ID, "password")
-password_field.send_keys("SuperSecretPassword!")
+password_field = driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
 
 # Находим кнопку Login и нажимаем её
 login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
